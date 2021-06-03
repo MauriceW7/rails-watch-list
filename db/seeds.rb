caf21b8b3897adb9ff307566932bb0710d24b15d
Maurice Welter <mauricewelter7@gmail.com>
2021-06-03 17:38:10 +0200

    Last one for today

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Movie.destroy_all
+
+50.times do
+  mov = Movie.new(
+    title: Faker::Movie.title,
+    overview: Faker::Movie.quote,
+    poster_url: 'https://picsum.photos/200/300',
+    rating: rand(0..100) / 10.0
+  )
+    mov.save(validate: false)
+  puts "Created #{mov.title}"
+end
+
+puts "Created #{Movie.count} restaurants"
